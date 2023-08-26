36a93b97767b071ca5923973f4d6c2971140d696
yangkun <171819480@qq.com>
2023-08-26 14:50:10 +0800

    Revert "Update try2.py"
    This reverts commit f925d18a37f5db3c66bb2a4a71bf2687abfe0cc5.

--- try2.py
+++ try2.py
@@ -90,4 +90,3 @@ json.dump(train_set, open('./try300/train_tasks.json', 'w'))
 json.dump(test_set, open('./try300/test_tasks.json', 'w'))
 json.dump(dev_set, open('./try300/dev_tasks.json', 'w'))
 
-def bubble_sort()
